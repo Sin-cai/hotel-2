6823f40da709186d300f0ca400b69d01cca20326
Sin-cai <134072094+Sin-cai@users.noreply.github.com>
2024-02-05 14:23:20 +0700

    Delete hotel directory

--- hotel/kamar/hapus.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include '../koneksi.php';
-
-    $kamar = $_GET ['id'];
-    mysqli_query($koneksi, "DELETE FROM kamar WHERE roomID='$kamar'");
-    header("location : index.php");
-?>
--- hotel/reserfasi/aksi-checkin.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../koneksi.php';
-
-$id_RS = $_POST['id_RS'];
-
-mysqli_query($koneksi, "INSERT INTO transaksi(id_rs) values ('$id_RS') ");
-header("location:../admin/login.php");
-?>
